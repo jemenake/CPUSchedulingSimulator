ffc0095a95fbdbe2ed54be7e09c8560a38521b5b
Baylor Whitehead <38015563+BaylorWhitehead@users.noreply.github.com>
2022-02-24 17:32:07 -0800

    Updating waits and compute times, deep copy logging jobs, auto formatted

--- simulator/sim.js
+++ simulator/sim.js
@@ -27,7 +27,7 @@ class Job {
             throw new Error("ERROR: You must call Job.isAlive with a cputime")
         }
         // If it's our arrival time and we haven't finished
-        return (this.arrival_time <= cpu_time) && (! this.isFinished())
+        return (this.arrival_time <= cpu_time) && (!this.isFinished())
     }
 
     isFinished() {
@@ -35,13 +35,13 @@ class Job {
     }
 
     isWaiting() {
-        return (! this.isFinished()) && (this.lifecycle[0][0] == "w")
+        return (!this.isFinished()) && (this.lifecycle[0][0] == "w")
     }
 
     // Decrement the wait timer
     recordWait() {
         // Make sure that we're really waiting
-        if(! this.isWaiting()) {
+        if (!this.isWaiting()) {
             throw new Error("ERROR: we were asked to record a wait for a non-waiting process, which looked like " + JSON.stringify(this))
         }
         // Get everything after the first char as an integer
@@ -56,7 +56,7 @@ class Job {
 
     recordComputation() {
         // Make sure that we're really waiting
-        if(this.isWaiting()) {
+        if (this.isWaiting()) {
             // This is actually something we shouldn't throw an exeption for. This would just indicate
             // a bad scheduler, so we should schedule it and just make note of the mistake in the notes
             // for this timeslice
@@ -69,7 +69,7 @@ class Job {
             delete this.lifecycle[0]
         } else {
             this.lifecycle[0] = "c" + (number - 1)
-        }    
+        }
     }
 }
 
@@ -103,29 +103,33 @@ class SystemState {
 
     dumpJobs() {
         this.jobs.forEach((job) => {
-            console.log(job.toString())
+            console.log(JSON.parse(JSON.stringify(job)))
         })
     }
-    
+
     dumpLiveJobs() {
         this.dumpJobs(this.getLiveJobs())
     }
-    
+
     dumpStatus() {
         console.log("======= Simulator Status =======")
         console.log("Clock:" + this.getSystemTime())
         console.log("  == Live Jobs ==")
         this.dumpLiveJobs()
     }
-    
+
     // Here's where we actually record the decision from the scheduler. We are passed
     // an array of processes which are assigned to CPU0, CPU1, etc, according to the
     // array index
     recordSchedule(assignments) {
-        for(var i=0; i<assignments.length; i++) {
-            console.log("CPU" + i + " gets process " + assignments[i])
-        }
-        this.trace.push(assignments.slice()) // Make a copy of the array, just in case something else messes with it later
+        // for(var i=0; i<assignments.length; i++) {
+        //     console.log("CPU" + i + " gets process " + assignments[i])
+        // }
+
+        // Makes a deep copy according to stack overflow
+        let clonedArray = JSON.parse(JSON.stringify(assignments))
+
+        this.trace.push(clonedArray.slice()) // Make a copy of the array, just in case something else messes with it later
     }
 }
 
@@ -137,9 +141,9 @@ class SystemState {
 
 function simulator() {
     var starting_jobs = [
-        new Job(0, 0, 3,[ "c3", "w2", "c4" ]),
-        new Job(1, 2, 3,[ "c8", "w12", "c24", "w1", "c1" ]),
-        new Job(2, 5, 3,[ "c22" ])
+        new Job(0, 0, 3, ["c3", "w2", "c4"]),
+        new Job(1, 2, 3, ["c8", "w12", "c24", "w1", "c1"]),
+        new Job(2, 5, 3, ["c22"])
     ]
 
     // Define the system we're running on
@@ -148,15 +152,15 @@ function simulator() {
     let system_state = new SystemState(starting_jobs)
 
     // Grab tasks that start at time zero if any
-    current_jobs = [] 
-    for(let job in starting_jobs){
-        if (job.arrival_time == 0){
+    current_jobs = []
+    for (let job in starting_jobs) {
+        if (job.arrival_time == 0) {
             current_jobs.push(job)
         }
     }
 
     // Here's where we make a list of every scheduler we want to run this job list against
-    let schedulers = [ new RandomScheduler() ]
+    let schedulers = [new RandomScheduler()]
 
     // At this point, we have a list of jobs, and we can cycle through all of the schedulers
     schedulers.forEach((scheduler) => {
@@ -170,39 +174,44 @@ function computeScheduleWith(system, system_state, scheduler) {
 
     // Run until nothing is alive
     //while (starting_jobs.some((job) => job.isAlive())) {
-    for(i=0; i<3; i++) {
+    for (i = 0; i < 3; i++) {
         let cpu_time = system_state.getSystemTime()
         cpu_assignments = scheduler.schedule(system, system_state)
-        // TODO: Decrease wait and cpu times based upon what the scheduler decided
-        //      for each process that the scheduler has selected for computation
-        //          recordComputation()        
-        //      for each process that is waiting
-        //          recordWait()
+
+        // Decrease wait and cpu times based upon what the scheduler decided
+        system_state.jobs.forEach((job) => {
+            if (job.number in cpu_assignments) {
+                job.recordComputation()  // Reduce computation
+            }
+            if (job.isWaiting()) {
+                job.recordWait()        // Reduce wait
+            }
+        })
 
         // Add these assignments to the overall schedule
         system_state.recordSchedule(cpu_assignments)
         system_state.dumpStatus()
     }
-    
-        
-        
-        // Based on result add to final schedule list for gui demo
-        // job_to_run.add_to_results()
-
-        // Prep info
-        // cycle++
-        // for(let job in starting_objs){
 
 
-        // Append to the_schedule whatever the process->CPU assignments are for this cycle            if (job.arrival_time == cycle){
-                // current_jobs.push(job)
+
+    // Based on result add to final schedule list for gui demo
+    // job_to_run.add_to_results()
+
+    // Prep info
+    // cycle++
+    // for(let job in starting_objs){
+
+
+    // Append to the_schedule whatever the process->CPU assignments are for this cycle            if (job.arrival_time == cycle){
+    // current_jobs.push(job)
     // Give the resulting schedule to the UI 
 
-        //    }
-        // }
-        // list_of_objects // pull in procceses that just arrived, update any computations done on this end
+    //    }
+    // }
+    // list_of_objects // pull in procceses that just arrived, update any computations done on this end
     // }
 }
-// 
+//
 
 // simulator()
